f1fda17f913314f9489f58bf261c2f7d5323bf21
Emmanuel-9 <oregadaniel181@gmail.com>
2025-05-13 18:49:03 +0300

    Add the CRM tracking log.

---
+++ crm_tracking_log/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'CRM Tracking Log',
+    'version': '1.0',
+    'category': 'CRM',
+    'summary': 'Track salesperson locations and link to CRM leads',
+    'depends': ['crm'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/tracking_log_views.xml',
+        'views/tracking_log_dashboard.xml',
+        'data/tracking_log_dashboard_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
