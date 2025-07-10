57d4eb4a0a9bc731e17bcbae2d8260dd157ff634
Emmanuel-9 <oregadaniel181@gmail.com>
2025-07-10 09:03:26 +0300

    Change the description.

--- tenant_rent_report/models/tenant_rent.py
+++ tenant_rent_report/models/tenant_rent.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class Tenant(models.Model):
     _name = 'tenant.rent'
-    _description = 'Tenant Rent Payment'
+    _description = 'Tenant Rent'
 
     name = fields.Char(string="Tenant Name", required=True)
     rent_amount = fields.Float(string="Rent Amount", required=True)
